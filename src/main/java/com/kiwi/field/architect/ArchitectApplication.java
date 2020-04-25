d17f2062c0d5f726e3f312b02e21356641614297
kiwi-field <909421773@qq.com>
2020-04-25 16:13:17 +0800

    架构师课程学习代码

---
+++ src/main/java/com/kiwi/field/architect/ArchitectApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.field.architect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiwi/field/architect/ArchitectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.field.architect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
